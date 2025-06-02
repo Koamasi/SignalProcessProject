3595eb9a3659fe66e21c5031b9cdef8424cf079a
BlazeCat <110224489+Koamasi@users.noreply.github.com>
2025-06-02 16:33:06 +0800

    添加小波变换与逆变换

--- util/wavelet_transform.py
+++ util/wavelet_transform.py
@@ -1,3 +1,13 @@
 import cv2
 import numpy as np
 import pywt
+
+
+def _coeffs(img, coeffs_name, level):
+    coeffs = pywt.wavedec2(cv2.imread(img, 0), str(coeffs_name), level=level)
+    return coeffs
+
+
+def _coeffs_re(coeffs, coeffs_name):
+    img = pywt.waverec2(coeffs, coeffs_name)
+    return img
