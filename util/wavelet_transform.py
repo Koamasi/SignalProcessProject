e2f2d9a380080498accbbf62bee5369336aac4a5
BlazeCat <110224489+Koamasi@users.noreply.github.com>
2025-06-02 22:23:16 +0800

    添加函数形参类型与返回值类型提示

--- util/wavelet_transform.py
+++ util/wavelet_transform.py
@@ -1,13 +1,15 @@
+from typing import Any
+
 import cv2
-import numpy as np
 import pywt
+import numpy as np
 
 
-def _coeffs(img, coeffs_name, level):
-    coeffs = pywt.wavedec2(cv2.imread(img, 0), str(coeffs_name), level=level)
+def _coeffs(imgPath: str, coeffs_name: str, level:int) -> list[Any]: #读取图像并进行小波分解
+    coeffs = pywt.wavedec2(cv2.imread(imgPath, 0), str(coeffs_name), level=level)
     return coeffs
 
 
-def _coeffs_re(coeffs, coeffs_name):
-    img = pywt.waverec2(coeffs, coeffs_name)
+def _coeffs_re(coeffs: list[Any], coeffs_name: str) -> np.ndarray: #逆小波分解并还原为图像
+    img = np.clip(pywt.waverec2(coeffs, coeffs_name), 0, 255).astype(np.uint8)
     return img
