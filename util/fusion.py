1d895e91068dfe14651c61baed7c743560d2877a
BlazeCat <110224489+Koamasi@users.noreply.github.com>
2025-06-03 21:08:55 +0800

    分离权重归一化函数并细化归一化

--- util/fusion.py
+++ util/fusion.py
@@ -1,15 +1,38 @@
+import warnings
 from typing import Any
 
 
 def ensureLowHigh(level: int) -> dict[str, list[Any]]: #确定并拆分低频与高频部分
+    warnings.warn('', DeprecationWarning)
     result = {'Low':[], 'High':[]}
     return result
 
+def normalize(weight: dict[str, list[float]]) -> dict[str, list[float]]: #权重归一化
+    result = {'Low': [], 'High': []}
 
-def advLowMaxHigh(coeffs: dict[str, list[Any]]) -> list[Any]: #低频平均高频取大策略
-    fused_coeffs = []
+    if sum(weight['Low']) == 1:
+        result['Low'] = result['Low'] + weight['Low']
+    else:
+        weight_sum_Low = sum(weight['Low'])
+        for i in weight['Low']:
+            result['Low'].append(i/weight_sum_Low)
+
+    if sum(weight['High']) == 1:
+        result['High'] = result['High'] + weight['High']
+    else:
+        weight_sum_High = sum(weight['High'])
+        for i in weight['High']:
+            result['High'].append(i/weight_sum_High)
+
+    return result
+
+
+def advLowMaxHigh(coeffs: dict[str, list[Any]], weight: dict[str, list[float]]) -> list[Any]: #自定义权重融合
+    fused_coeffs = []; weight_norm = normalize(weight)
 
     for i in range(len(coeffs)):
         if i:
-            fused_coeffs.append([np.maximum(coeffs[i][j], coeffs[i][j]) for j in range(len(coeffs[i]))])
+            fused_coeffs.append(
+
+            )
     return fused_coeffs
