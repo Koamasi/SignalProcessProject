7ecbd0c93a0cedbfdee05614b5d21a8e4b66f75a
BlazeCat <110224489+Koamasi@users.noreply.github.com>
2025-06-11 16:48:43 +0800

    删除类 改为全局 goto未完成

--- menu.py
+++ menu.py
@@ -1,44 +1,32 @@
 import warnings
 from tkinter import Tk, filedialog
+from typing import Any
+from goto import with_goto
+
 import pywt
-from terminal_layout.extensions.choice import StringStyle, Choice
-from terminal_layout import Fore
+from util import menu_tool as mt
 
 
 # TODO 弃用类 需要获取多图路径 分解方案 小波名称 分解层数 融合方案 若自定义融合需获取权重
-# TODO 可视化进度 存在确认阶段
-class Menu:
-    warnings.warn('', DeprecationWarning)
-    wave_name = pywt.families()
-    wave_choice = Choice(
-        'Please select wavelet',
-        wave_name,
-        icon_style=StringStyle(fore=Fore.lightblue),
-        selected_style=StringStyle(fore=Fore.lightblue)
-    ).get_choice()
-    wave_choice_optional = Choice(
-        'Please select second wavelet',
-        wave_name.append('No more wavelet needed'),
-        icon_style=StringStyle(fore=Fore.lightblue),
-        selected_style=StringStyle(fore=Fore.lightblue)
-    ).get_choice()
-    window = Tk()
-    window.withdraw()
-    filePath = filedialog.askopenfilename()
+# TODO 可视化进度 存在确认阶段 预设（未完成
+menu_control = mt.menu_questions
 
 
-    def menu(self):
-        result = {
-            'waveChoice': self.wave_choice[1],
-            'waveChoiceOptional': None if self.wave_choice_optional == 'No more wavelet needed' else self.wave_choice_optional[1],
-            'filePath': self.filePath
-        }
+@with_goto
+def menu_start() -> list[Any]:
+    result = []
 
-        return result
+    label .decomposition
+    decomposition = menu_control.decomposition_select.get_choice()
 
+    match int(decomposition[1]):
+        case 1:
+            wavelet = menu_control.wavelet_select.get_choice()
+        case 2:
+            print('尚未完成'); goto .decomposition
 
-wave_name = pywt.families()
+    return result
 
 
 if __name__ == '__main__':
-    Menu().menu()
+    menu_start()
